4066f7b9bf59af439d57e40a7339a9949f6a63dd
Hoàng Việt Trung <trunghoang220703@gmail.com>
2024-08-21 11:29:28 +0700

    first release

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bai1 
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Giai tich, hieu, tong cua 2 so");
+            Console.Write("nhap so thu nhat: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("nhap so thu hai: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a + b;
+            int difference = a - b;
+            double product = a * b;
+
+            Console.WriteLine("Tong 2 so la: " + sum);
+            Console.WriteLine("hieu 2 so la: " + difference);
+            Console.WriteLine("Tich 2 so laL "+ product);
+        }
+    }
+}
---
+++ bai2/myproject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HelloWorld
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("giai phuong trinh bac 1");
+            Console.Write("nhap a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nhap b: ");
+            double b = Convert.ToDouble(Console.ReadLine()); 
+            double x = -b/a;
+            Console.WriteLine("Ket qua: " + x);
+           
+        }
+   }
+} 
