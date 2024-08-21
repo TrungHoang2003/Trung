785be09e2b58ae28bbc2832a49312303c89ee302
Hoàng Việt Trung <trunghoang220703@gmail.com>
2024-08-21 18:30:46 +0700

    first release

---
+++ bai3/bai3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c;
+            Console.Write("Nhap C: ");
+            while (true)
+            {
+                if (double.TryParse(Console.ReadLine(), out c))
+                     break;
+                Console.Write("Du lieu khong hop le, nhap lai: ");
+            }
+            double k = c + 273; 
+            Console.WriteLine("K: " + k);
+            double f = (c * 18 / 10) + 32;
+            Console.WriteLine("F: " + f);
+        }
+    }
+}
