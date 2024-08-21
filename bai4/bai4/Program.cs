002cde8c785cc8d561f1ff2f801fac435b23c2fe
Hoàng Việt Trung <trunghoang220703@gmail.com>
2024-08-21 22:30:31 +0700

    first Release

---
+++ bai4/bai4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bai4
+{
+    class Program
+    {
+        static double giaiThua(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            else
+                return n*giaiThua(n-1);
+        }
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while(true)
+            {
+                Console.Write("Nhap so nguyen duong n: ");
+                if (int.TryParse(Console.ReadLine(), out n))
+                    break;
+                Console.Write("Du lieu khong hop le, nhap lai: ");
+            }    
+            Console.WriteLine("Giai thua cua " + n + " la: " + giaiThua(n));
+        }
+    }
+}
