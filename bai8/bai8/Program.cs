828c856c239df5e5577b36e4c07d6e5104916430
Hoàng Việt Trung <trunghoang220703@gmail.com>
2024-08-22 10:32:01 +0700

    first release

---
+++ bai8/bai8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Bai8
+{
+    class Program
+    {
+        static int input(int n)
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out n))
+                    break;
+                Console.Write("Du lieu khong hop le, nhap lai: ");
+            }
+            return n;
+        }
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("Nhap so phan tu cua mang: ");
+            n = input(n);
+
+            int[] arr = new int[n]; 
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap phan tu thu " + (i+1) + ": ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());   
+            }
+            Console.Write("Cac phan tu le cua mang: ");
+            for(int i = 0;i < n; i++)
+            {
+                if (arr[i] % 2 !=0)
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Cac phan tu chan cua mang: ");
+            for(int i = 0;i < n; i++)
+            {
+                if (arr[i] % 2 ==0)
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ bai9/bai9/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Bai9
+{
+    class Program
+    {
+        static int input(int n)
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out n))
+                    break;
+                Console.Write("Du lieu khong hop le, nhap lai: ");
+            }
+            return n;
+        }
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("Nhap so phan tu cua mang: ");
+            n = input(n);
+
+            int[] arr = new int[n];
+
+            for (int i=0;i<n;i++)
+            {
+                Console.Write("Nhap phan tu thu " + (i+1) + ": ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j = i+1; j < n; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = 0;
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.Write("Day sap xep tang dan: ");
+            for (int i = 0; i < n; i++)
+            {
+                    Console.Write(arr[i] + " ");
+            }
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        int temp = 0;
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Day sap xep giam dan: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+        // 2 9 8 1
+    }
+}
