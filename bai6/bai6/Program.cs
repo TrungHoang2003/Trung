55e512ebca3a8a500a265a6d83623f46110cbcf2
Hoàng Việt Trung <trunghoang220703@gmail.com>
2024-08-22 09:00:20 +0700

    first release

---
+++ bai6/bai6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bai6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap mot so nguyen bat ki: ");
+            int n;
+            while(true)
+            {
+                if (int.TryParse(Console.ReadLine(), out n))
+                    break;
+                Console.Write("n khong phai la so nguyen, vui long nhap lai: ");
+            }
+            if (n % 2 == 0)
+                Console.WriteLine(n + " la so chan.");
+            else
+                Console.WriteLine(n + " la so le.");
+        }
+    }
+}
