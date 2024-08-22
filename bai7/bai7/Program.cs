21050c06083e079efd142d713ef4fcba659ddc15
Hoàng Việt Trung <trunghoang220703@gmail.com>
2024-08-22 09:11:15 +0700

    first release

---
+++ bai7/bai7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Bai4
+{
+    class Program
+    {
+        static bool KiemTraSoNguyenTo(int n)
+        {
+            if (n <= 1) return false;
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen duong n: ");
+            int n;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out n))
+                    break;
+                Console.WriteLine("Vui long nhap so nguyen duong:");
+            }
+
+            if (n <= 1)
+                Console.WriteLine(n + " khong phai la so nguyen to.");
+            else
+            {
+                bool isPrime = KiemTraSoNguyenTo(n);
+                if (isPrime)
+                {
+                    Console.WriteLine(n + " la so nguyen to");
+                }
+                else
+                    Console.WriteLine(n + " khong phai la so nguyen to");
+            }
+
+        }
+    }
+}
